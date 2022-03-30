10524903c31c7d297368e6336a42489c2a6550b6
Priyadarshi Kumar <priyadarshigeorgian@gmail.com>
2022-03-30 20:12:38 +0530

    delete a task group now

--- src/components/TakGroup/index.jsx
+++ src/components/TakGroup/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import TaskCard from '../TaskCard'
+import useStore from "./../../store/context"
 import "./styles.css"
 
 const TaskGroup = ({task: {tasks, title}, id}) => {
+  const [_, dispatch] = useStore();
+  const deleteTaskGroup = () => {
+    dispatch({
+      type: "DELETE_TASK_GROUP",
+      payload: id
+    })
+  }
   return (
     <div className='taskGroup'>
       <h3>{title}</h3>
       {tasks.map((task, taskId) => <TaskCard task={task} key={taskId}></TaskCard>)}
+      <button onClick={deleteTaskGroup}>Delete Task Group</button>
     </div>
   )
 }
--- src/store/reducers.jsx
+++ src/store/reducers.jsx
@@ -1,4 +1,5 @@
 export const reducer = (state, action) => {
+    console.log("reducer called")
     switch(action.type) {
         case "ADD_TASK_GROUP":
             return [...state, {title: "New Task Group", tasks: []}]
@@ -8,6 +9,10 @@ export const reducer = (state, action) => {
             taskGroup.tasks.push(data);
             state.splice(idx, 1, taskGroup);
             return [...state, ]
+        case "DELETE_TASK_GROUP":
+            const id = action.payload;
+            state.splice(id, 1);
+            return [...state]
         default: 
             return state;
     }
